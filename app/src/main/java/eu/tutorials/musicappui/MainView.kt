07ba7afd111fbeb10716f81952908d2592db6943
Elif İrem Kara Kadyrov <83959541+elfrmkr@users.noreply.github.com>
2025-01-13 12:53:31 +0100

    feat: made the drawer visible

--- app/src/main/java/eu/tutorials/musicappui/MainView.kt
+++ app/src/main/java/eu/tutorials/musicappui/MainView.kt
@@ -1,6 +1,14 @@
 package eu.tutorials.musicappui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
@@ -8,12 +16,22 @@ import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
@@ -23,6 +41,14 @@ fun MainView() {
 
     val scaffoldState: ScaffoldState = rememberScaffoldState()
     val scope: CoroutineScope = rememberCoroutineScope()
+
+    // allows to determine which Screen we are on currently
+    val controller: NavController = rememberNavController()
+    val navBackStackEntry by controller.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+    val title = remember{
+        mutableStateOf("")
+    }
 
     Scaffold(
         topBar = {
@@ -37,8 +63,59 @@ fun MainView() {
                     Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "Menu" )
                 }
             })
+        },
+        scaffoldState = scaffoldState,
+        drawerContent = {
+            LazyColumn(Modifier.padding(16.dp) ) {
+                items(screensInDrawer) {
+                    item ->
+                    DrawerItem(selected = currentRoute == item.dRoute, item = item) {
+                        scope.launch {
+                            scaffoldState.drawerState.close()
+                        }
+                        if(item.dRoute == "add_account") {
+                            //open dialog
+                        } else {
+                            controller.navigate(item.dRoute)
+                            title.value = item.dTitle
+                        }
+                    }
+                }
+            }
         }
     ) {
         Text(text = "Text", modifier = Modifier.padding(it))
+    }
+}
+
+
+@Composable
+fun DrawerItem(
+    selected:Boolean,
+    item: Screen.DrawerScreen,
+    onDrawerItemClicked: () -> Unit
+) {
+    val background = if(selected) Color.Gray else Color.White
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp, vertical = 16.dp)
+            .background(background)
+            .border(
+                width = 1.dp,
+                color = background,
+                shape = RoundedCornerShape(5.dp)
+            )
+            .clickable {
+                onDrawerItemClicked()
+            }
+
+    ) {
+        Icon(
+            painter = painterResource(id = item.icon),
+            contentDescription = item.dTitle,
+            Modifier.padding(end = 8.dp, top = 4.dp))
+
+        Text(text = item.dTitle, style = MaterialTheme.typography.h5)
     }
 }
